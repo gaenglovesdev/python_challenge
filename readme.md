8ebfca8aefea5125d754674e88cc50e2b07bb9d2
gaeng <gaengsworld@gmail.com>
2020-11-16 00:01:42 +0900

    파일명 날짜 추가 및  readme checklist

--- readme.md
+++ readme.md
@@ -37,6 +37,6 @@ _"Life is too short, You need python." (인생은 너무 짧으니 파이썬이 
 ## GOAL
 
 - [x] Syntax
-- [ ] Scrapper
+- [x] Scrapper
 - [ ] Django
 - [ ] Tensorflow
--- scrapper/save_data.py
+++ scrapper/save_data.py
@@ -1,8 +1,10 @@
 import csv
+from datetime import datetime
 
 
 def save_to_file(data):
-    file = open("scrapper/파이썬_개발자_공고.csv", "w")
+    today = str(datetime.today()).split(" ")[0].replace("-", "_")
+    file = open(f"scrapper/{today}_잡코리아_파이썬_개발자_공고.csv", "w")
     writer = csv.writer(file)
     writer.writerow(["회사명", "내용", "링크"])
     for job in data:
